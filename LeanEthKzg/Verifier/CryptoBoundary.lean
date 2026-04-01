a5807eaaf768c0e49c2998892a7c450a2d44c8b9
junssbo1238 <parksj0106@gmail.com>
2026-04-02 00:36:36 +0900

    Add blob proof boundary requirements

--- LeanEthKzg/Verifier/CryptoBoundary.lean
+++ LeanEthKzg/Verifier/CryptoBoundary.lean
@@ -18,6 +18,8 @@ abbrev ProofDecodePredicate := ProofBytes -> Prop
 
 abbrev FieldElementCanonicalPredicate := FieldElementBytes -> Prop
 
+abbrev BlobFieldElementsCanonicalPredicate := BlobBytes -> Prop
+
 abbrev CommitmentSubgroupPredicate := CommitmentBytes -> Prop
 
 abbrev ProofSubgroupPredicate := ProofBytes -> Prop
@@ -43,6 +45,26 @@ def KzgProofBoundaryRequirements.holdsFor
   requirements.proofDecodes input.proof /\
   requirements.evaluationPointCanonical input.evaluationPoint /\
   requirements.claimedValueCanonical input.claimedValue /\
+  requirements.commitmentInSubgroup input.commitment /\
+  requirements.proofInSubgroup input.proof /\
+  requirements.commitmentNotInfinity input.commitment /\
+  requirements.proofNotInfinity input.proof
+
+structure BlobProofBoundaryRequirements where
+  blobFieldElementsCanonical : BlobFieldElementsCanonicalPredicate
+  commitmentDecodes : CommitmentDecodePredicate
+  proofDecodes : ProofDecodePredicate
+  commitmentInSubgroup : CommitmentSubgroupPredicate
+  proofInSubgroup : ProofSubgroupPredicate
+  commitmentNotInfinity : CommitmentNotInfinityPredicate
+  proofNotInfinity : ProofNotInfinityPredicate
+
+def BlobProofBoundaryRequirements.holdsFor
+    (requirements : BlobProofBoundaryRequirements)
+    (input : NormalizedBlobProofInput) : Prop :=
+  requirements.blobFieldElementsCanonical input.blob /\
+  requirements.commitmentDecodes input.commitment /\
+  requirements.proofDecodes input.proof /\
   requirements.commitmentInSubgroup input.commitment /\
   requirements.proofInSubgroup input.proof /\
   requirements.commitmentNotInfinity input.commitment /\
